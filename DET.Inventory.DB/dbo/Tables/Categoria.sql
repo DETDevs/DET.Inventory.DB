13f32d08ead33c10e60bdc5217760f221cf987c9
douglas-i <hdouglasu@gmail.com>
2025-02-02 15:57:50 -0600

    t-dev: add tables

---
+++ DET.Inventory.DB/dbo/Tables/Categoria.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Categoria] (
+    IdCategoria         INT PRIMARY KEY IDENTITY(1,1),
+    Codigo              VARCHAR(255) NOT NULL,
+    Nombre              VARCHAR(100) NOT NULL,
+    Descripcion         VARCHAR(255),
+    Activo              BIT DEFAULT 1,
+    FechaCreacion       DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UsuarioCreacion     VARCHAR(255) NOT NULL,
+    FechaModificacion   DATETIME
+);
---
+++ DET.Inventory.DB/dbo/Tables/Persona.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Persona]
+(
+	IdPersona           INT PRIMARY KEY IDENTITY(1,1),
+    Nombre              VARCHAR(100) NOT NULL,
+    Apellidos           VARCHAR(100),
+    Identificacion      VARCHAR(50) UNIQUE,
+    Telefono            VARCHAR(20),
+    Direccion           VARCHAR(255),
+    Email               VARCHAR(100),
+    Activo              BIT DEFAULT 1,
+    FechaCreacion       DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UsuarioCreacion     VARCHAR(255) NOT NULL,
+    FechaModificacion   DATETIME
+)
